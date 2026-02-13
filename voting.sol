45c7e3afc42916bbe7a89662c68320021da6ac60
AlamuriPavanKumar <alamuripavankumar@gmail.com>
2026-02-13 20:42:19 +0530

    Add files via upload

---
+++ voting.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Voting {
+
+    struct Candidate {
+        string name;
+        uint voteCount;
+    }
+
+    mapping(address => bool) public hasVoted;
+
+    Candidate[] public candidates;
+
+    constructor() {
+        candidates.push(Candidate("Alice", 0));
+        candidates.push(Candidate("Bob", 0));
+    }
+
+    function vote(uint candidateIndex) public {
+        require(!hasVoted[msg.sender], "Already voted");
+
+        hasVoted[msg.sender] = true;
+        candidates[candidateIndex].voteCount++;
+    }
+
+    function getVotes(uint candidateIndex) public view returns(uint) {
+        return candidates[candidateIndex].voteCount;
+    }
+}
